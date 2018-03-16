3174dfeb6d5ec9618a29aa3ef962e56aa64c7cc9
Cami Rodriguez <camiepisode@outlook.com>
2018-03-16 11:05:24 -0500

    improved code

--- src/manage/components.js
+++ src/manage/components.js
@@ -1,11 +1,17 @@
 components = {
-  frontend: [
+  frontendComponents: [
     { route: '/', view: '/home/home.route', action: 'index' },
     { route: '/robots.txt', view: '/seo/seo.route', action: 'robots' },
     { route: '/sitemap/master.xml', view: '/seo/seo.route', action: 'sitemapMaster' },
     { route: '/sitemap/latest.xml', view: '/seo/seo.route', action: 'sitemapLatest' },
   ],
-  api: [
+  apiComponents: [
+    { route: '/Status', controller: '/status/status.route', action: 'get', method: 'GET' },
+    { route: '/Status', controller: '/status/status.route', action: 'get', method: 'GET' },
+    { route: '/Status', controller: '/status/status.route', action: 'get', method: 'GET' },
+    { route: '/Status', controller: '/status/status.route', action: 'get', method: 'GET' },
+    { route: '/Status', controller: '/status/status.route', action: 'get', method: 'GET' },
+    { route: '/Status', controller: '/status/status.route', action: 'get', method: 'GET' },
     { route: '/Status', controller: '/status/status.route', action: 'get', method: 'GET' },
   ]
 }
