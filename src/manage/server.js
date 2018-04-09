1f1109213f01b10038332812074e4a1b5402a11b
thEpisode <camiepisode@outlook.com>
2018-04-09 03:26:00 -0500

    improved server notation

--- src/manage/server.js
+++ src/manage/server.js
@@ -17,46 +17,13 @@ function server (args) {
     if (port >= 0) return port
     return false
   }
-
-  const responseError = function (message) {
-    if (message) {
-      return { success: false, message: message, result: null }
-    } else {
-      return { success: false, message: 'Something was wrong while you make this action', result: null }
-    }
-  }
-
-  const responseSuccess = function (data, message) {
-    if (message) {
-      return {
-        success: true,
-        message: message,
-        result: data
-      }
-    } else {
-      return {
-        success: true,
-        message: 'Operation completed succesfuly',
-        result: data
-      }
-    }
-  }
-
-  const sendBadRequestView = function (req, res) {
-    res.render('maintenance/maintenance.view.jsx', null)
-  }
-
+  
   const getSettings = () => {
     return settings
   }
 
   return {
     start: startServer,
-    response: {
-      error: responseError,
-      success: responseSuccess,
-      badRequestView: sendBadRequestView
-    },
     settings: getSettings
   }
 }
