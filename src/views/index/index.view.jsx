5daca7804ab5ba1342a7e1a5f262518e2a63cc07
thEpisode <camiepisode@outlook.com>
2018-04-19 01:41:12 -0500

    included missed token

--- src/views/index/index.view.jsx
+++ src/views/index/index.view.jsx
@@ -9,7 +9,7 @@ class Index extends React.Component {
       <Layout {...this.props}>
         <section class="jumbotron text-center">
           <div class="container">
-            <h1 class="jumbotron-heading">Beat</h1>
+            <h1 class="jumbotron-heading">%BEAT% project</h1>
             <p class="lead text-muted">Server framework for your Node.js projects made with Express, Firebase and ReactDOM.</p>
             <a href="https://github.com/thEpisode/beat/wiki" class="btn btn-primary my-2 mx-2" target="_blank">Documentation</a>
             <a href="https://github.com/thEpisode/beat-cli" class="btn btn-secondary my-2 mx-2" target="_blank">beat-cli</a>
