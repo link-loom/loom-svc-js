ebf1848c0f23af51ebe9ef2ced342c4c0d30393f
Cami Rodriguez <camiepisode@outlook.com>
2018-07-16 21:27:51 -0500

    included controller manager

---
+++ src/core/controllerManager.js
@@ -0,0 +1,33 @@
+function ControllerManager (dependencies) {
+  /// Dependencies
+  const _console = dependencies.console
+  const { lstatSync, readdirSync } = require('fs')
+  const { join } = require('path')
+
+  /// Properties
+  let _controllers = {}
+
+  try {
+    // Read all directories in controllers folder
+    const isDirectory = source => lstatSync(source).isDirectory()
+    const getDirectories = source =>
+      readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+
+    const directories = getDirectories(`${dependencies.root}/src/controllers/`)
+
+    // Map all controllers
+    directories.map((path) => {
+      if (path) {
+        let name = path.split('\\')[path.split('\\').length - 1]
+        let pathName = `${path}\\${name}Controller`
+        _controllers[name] = require(pathName)(dependencies)
+      }
+    })
+  } catch (error) {
+    _console.error(error)
+  }
+
+  return _controllers
+}
+
+module.exports = ControllerManager
