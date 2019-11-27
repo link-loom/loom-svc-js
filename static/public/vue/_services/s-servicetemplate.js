7750686e9ccbc1691aa86e89e3364c87591342b4
Camilo Rodriguez <camiepisode@outlook.com>
2019-11-27 00:09:11 -0500

    improved service template and corrected linter

--- static/public/vue/_services/s-servicetemplate.js
+++ static/public/vue/_services/s-servicetemplate.js
@@ -7,21 +7,86 @@ var entityService = {
   },
   mounted () {
     this.services.entity = {
-      foo: this.$_foo_entity
+      getAll: this.$_getAll_entity,
+      getByParameters: this.$_getByParameters_entity,
+      update: this.$_update_entity,
+      create: this.$_create_entity
     }
   },
   methods: {
-    async $_foo_entity (data) {
+    async $_getAll_entity (data) {
       try {
-        if (!data || !data.userId) {
+        const result = await this.$http
+          .get('/api/entity/',
+            {
+              headers: {
+                'x-access-token': window.context.token
+              }
+            })
+
+        return result.body
+      } catch (error) {
+        console.error(error)
+        return error.body
+      }
+    },
+    async $_getByParameters_entity (data) {
+      try {
+        if (!data) {
           return null
         }
 
-        const result = await this.$http.get(`/api/fooEntity/getByIdentity/${data.userId}`, {
-          headers: {
-            'x-access-token': window.context.token
-          }
-        })
+        const parameters = this.objectToQueryString(data)
+
+        const result = await this.$http
+          .get(`/api/entity${parameters}`,
+            {
+              headers: {
+                'x-access-token': window.context.token
+              }
+            })
+
+        return result.body
+      } catch (error) {
+        console.error(error)
+        return error.body
+      }
+    },
+    async $_create_entity (data) {
+      try {
+        if (!data) {
+          return null
+        }
+
+        const result = await this.$http
+          .post('/api/entity/',
+            data,
+            {
+              headers: {
+                'x-access-token': window.context.token
+              }
+            })
+
+        return result.body
+      } catch (error) {
+        console.error(error)
+        return error.body
+      }
+    },
+    async $_update_entity (data) {
+      try {
+        if (!data) {
+          return null
+        }
+
+        const result = await this.$http
+          .put('/api/entity/',
+            data,
+            {
+              headers: {
+                'x-access-token': window.context.token
+              }
+            })
 
         return result.body
       } catch (error) {
