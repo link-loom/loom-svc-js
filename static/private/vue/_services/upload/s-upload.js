bf90eaa30086af2c16a4ffd9449ba651ef1212b4
Camilo Rodriguez <camiepisode@outlook.com>
2019-12-06 12:52:56 -0500

    improved upload service

--- static/private/vue/_services/upload/s-upload.js
+++ static/private/vue/_services/upload/s-upload.js
@@ -1,3 +1,4 @@
+/* global FormData */
 // eslint-disable-next-line no-unused-vars
 var uploadService = {
   data: {
@@ -7,21 +8,54 @@ var uploadService = {
   },
   mounted () {
     this.services.upload = {
-      photo: this.$_photo_upload
+      file: this.$_file_upload,
+      bulk: this.$_bulk_upload
     }
   },
   methods: {
-    async $_photo_upload (data) {
+    async $_file_upload (data) {
       try {
-        if (!data || !data.userId) {
+        if (!data) {
           return null
         }
 
-        const result = await this.$http.get(`/api/TODO/${data.userId}`, {
-          headers: {
-            'x-access-token': window.context.token
-          }
-        })
+        const formData = new FormData()
+        formData.append('file', this.data)
+
+        const result = await this.$http
+          .post('/api/upload/file',
+            formData,
+            {
+              headers: {
+                'Content-Type': 'multipart/form-data',
+                'x-access-token': window.context.token
+              }
+            })
+
+        return result.body
+      } catch (error) {
+        console.error(error)
+        return error.body
+      }
+    },
+    async $_bulk_upload (data) {
+      try {
+        if (!data) {
+          return null
+        }
+
+        const formData = new FormData()
+        formData.append('file', this.data)
+
+        const result = await this.$http
+          .post('/api/upload/bulk',
+            formData,
+            {
+              headers: {
+                'Content-Type': 'multipart/form-data',
+                'x-access-token': window.context.token
+              }
+            })
 
         return result.body
       } catch (error) {
