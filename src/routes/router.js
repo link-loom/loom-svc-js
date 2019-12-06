33a6cb212f8c20883dceb7d6002b0c9bcf1c335e
Camilo Rodriguez <camiepisode@outlook.com>
2019-12-06 12:53:24 -0500

    renamed route

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,7 +2,7 @@ const router = {
   api: [
     { httpRoute: '/status', route: '/routes/api/status/status.route', handler: 'get', method: 'GET', protected: false },
     // Upload files
-    { httpRoute: '/upload', route: '/routes/api/upload/upload.route', handler: 'upload', method: 'POST', protected: false, isUpload: true },
+    { httpRoute: '/upload/file', route: '/routes/api/upload/upload.route', handler: 'upload', method: 'POST', protected: false, isUpload: true },
     { httpRoute: '/upload/bulk', route: '/routes/api/upload/upload.route', handler: 'bulk', method: 'POST', protected: false, isUpload: true },
     // Auth
     { httpRoute: '/login', route: '/routes/api/auth/auth.route', handler: 'login', method: 'POST', protected: false },
