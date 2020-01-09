8ce59f892bbaf7b57bbb7af48b877467eaedd8f3
Camilo Rodriguez <camiepisode@outlook.com>
2020-01-08 22:28:16 -0500

    Improved maintenance view

--- src/views/shared/footer/_footerError.jsx
+++ src/views/shared/footer/_footerError.jsx
@@ -5,8 +5,14 @@ class Footer extends React.Component {
   render () {
     return (
 
-      <footer>
-        {/* TODO: */}
+      <footer className="row mt-5">
+        <div className="col-12 mt-xl-2">
+          <span className="text-white font-weight-medium text-center">Copyright © {date.getFullYear()} <a href="https://www.virtualcapitalofamerica.com/" target="_blank" className="text-white">Virtual capital of America</a>. All rights reserved.</span>
+        </div>
+
+        <div className="col-12 mt-xl-2">
+          <span className="text-white font-weight-medium text-center">Help <i className="mdi mdi-comment-question text-info"></i></span>
+        </div>
       </footer>
 
     );
