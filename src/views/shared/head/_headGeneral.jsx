009afa2b387a8e0825ba7e7993b1d5eba11eef58
Camilo Rodriguez <camiepisode@outlook.com>
2020-04-15 16:29:53 -0500

    included locale template

--- src/views/shared/head/_headGeneral.jsx
+++ src/views/shared/head/_headGeneral.jsx
@@ -6,7 +6,7 @@ class Head extends React.Component {
 
     return (
       <head>
-        <title>{`${this.props.title} -` || ''} %BEAT%</title>
+        <title>{`${this.props.data.locale.PAGE_TITLE} -` || ''} %BEAT%</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="keywords" content="some, words, about, this, project" />
