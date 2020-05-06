4598cae43daef5bad2e3dbba27c3753dde4c90e7
Pablo Andrés Dorado Suárez <pablo@valiu.co>
2020-05-06 18:06:15 -0500

    fix(Core:ControllerManager): Avoid locking separators to a single platform.

--- src/core/controller.manager.js
+++ src/core/controller.manager.js
@@ -8,7 +8,7 @@ class ControllerManager {
 
   loadControllers () {
     const { lstatSync, readdirSync } = require('fs')
-    const { join } = require('path')
+    const { join, basename } = require('path')
     this._controllers = {}
 
     try {
@@ -17,14 +17,14 @@ class ControllerManager {
       const getDirectories = source =>
         readdirSync(source).map(name => join(source, name)).filter(isDirectory)
 
-      const directories = getDirectories(`${this._dependencies.root}/src/controllers/`)
+      const directories = getDirectories(join(this._dependencies.root, 'src', 'controllers'))
 
       // Map all controllers
       directories.map((path) => {
         try {
           if (path) {
-            const name = path.split('\\')[path.split('\\').length - 1]
-            const pathName = `${path}\\${name}.controller`
+            const name = basename(path)
+            const pathName = join(path, `${name}.controller`)
             // self and dynamic propagation
             this._dependencies.controllers = this._controllers
             this._controllers[name] = require(pathName)(this._dependencies)
