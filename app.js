fe41d40a31545e41f741cd9e7dea1e018e817e13
Camilo Rodriguez <camiepisode@outlook.com>
2020-07-11 14:07:09 -0500

    fix: restored code

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 /**
- * Boiler Plate Node.js App
- * v0.1.0
+ * Main start
+ *
+ * To read documentation go to https://github.com/thepisode/beat/wiki
  */
 
-const server = require('./src/core/server')({ root: __dirname })
-server.start((dependencies) => {
+const { ServerManager } = require('./src/core/server.manager')
+const server = new ServerManager({ root: __dirname })
+
+const main = async () => {
+  const dependencies = await server.loadServer()
+
   // Launching server
-  dependencies.console.info(`http://localhost:${dependencies.config.SERVER_PORT}`, 'Server')
+  dependencies.console.info(`http://localhost:${dependencies.config.SERVER_PORT}/`, 'Server')
+  dependencies.console.info(`http://localhost:${dependencies.config.SERVER_PORT}/api/`, 'Server')
   dependencies.console.info(`${dependencies.config.SERVER_NAME} v${dependencies.config.SERVER_VERSION}`, 'Server')
   dependencies.console.log(' ------------------------------------')
-})
+}
+
+main()
