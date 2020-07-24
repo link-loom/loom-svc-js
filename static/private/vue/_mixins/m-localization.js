e218f48c72d990715285e7689658be0c6e35cd21
Camilo Rodriguez <camiepisode@outlook.com>
2020-07-24 02:23:17 -0500

    fix: corrected bugs on localization

--- static/private/vue/_mixins/m-localization.js
+++ static/private/vue/_mixins/m-localization.js
@@ -1,27 +1,26 @@
-/* global $ */
 // eslint-disable-next-line no-unused-vars
 var localization = {
+  created: function () {
+    this.setAllLocalizationLinks()
+  },
   methods: {
     setAllLocalizationLinks () {
-      var lang = this.$cookies.get('lang')
+      const lang = this.$cookies.get('lang')
 
       if (!lang) { return }
 
-      $('a').each(function () {
-        if ($(this).attr('href')[0] === '/') {
-          $(this).attr('href', '/' + lang + $(this).attr('href'))
-        }
-      })
-    },
-    langListOnClick (event) {
-      if (event) { event.preventDefault() }
+      const aTags = document.querySelectorAll('a[translate]')
 
-      $('.vhq-dropdown-selector-list').toggleClass('vhq-dropdown-selector-open')
+      for (const aTag of aTags) {
+        if (aTag.pathname === '/') {
+          aTag.pathname += 'index'
+        }
+
+        aTag.href = aTag.origin + '/' + lang + aTag.pathname
+      }
     },
     langItemOnClick (event) {
       if (event) { event.preventDefault() }
-
-      this.langListOnClick()
 
       var lang = event.currentTarget.dataset.lang || 'en'
       var slashCounts = window.location.pathname.split('').filter(function (l) { return l === '/' }).length
@@ -30,7 +29,7 @@ var localization = {
       if (slashCounts > 2) {
         window.location.assign('/' + lang + window.location.pathname.substring(3, window.location.pathname.length))
       } else {
-        window.location.assign('/' + lang + '/')
+        window.location.assign('/' + lang + '/index')
       }
     }
   }
