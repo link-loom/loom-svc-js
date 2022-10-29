424961603c166cecce7fe86f88ec56d0a8ac912a
Camilo Rodriguez <camiepisode@outlook.com>
2022-10-29 10:14:48 -0500

    feat: organized template

--- src/models/index.js
+++ src/models/index.js
@@ -2,5 +2,5 @@ module.exports.Notification = require('./notification/notification.model')
 module.exports.User = require('./user/user.model')
 module.exports.Device = require('./device/device.model')
 
-module.exports.Template = require('./template/template.model')
+module.exports.Template = require('./_template/_template.model')
 // FIXME: Include all models here
