7e9da04445c21e04995c6af8eca9e43ae05dff6f
Camilo Rodriguez <camiepisode@outlook.com>
2022-12-05 16:04:42 -0500

    Fix: Corrected bug in event emision

--- src/core/utils/event.util.js
+++ src/core/utils/event.util.js
@@ -3,29 +3,38 @@ class EventUtil {
     this._dependencies = dependencies
   }
 
-  #brokerEmitSingle ({ socket, settings, payload }) {
+  brokerEmitToSocket ({ socket, settings, payload }) {
     socket.emit(settings.name, payload)
   }
 
-  #brokerEmitTopics ({ websocketServer, settings, payload }) {
+  brokerEmitToTopics ({ websocketServer, settings, payload }) {
+    if (!payload.context.topics || !payload.context.topics.length) {
+      payload.context.topics = settings.topics
+    }
+
     for (const topic of payload.context.topics) {
       websocketServer.to(topic).emit(settings.name, payload)
     }
   }
 
   brokerEmit ({ websocketServer, socket, settings, payload }) {
-    if (!payload.context.topics || !payload.context.topics.length) {
-      this.#brokerEmitSingle({ socket, settings, payload })
-
+    if (!socket) {
+      this.brokerEmitToTopics({ websocketServer, settings, payload })
       return
     }
 
-    this.#brokerEmitTopics({ websocketServer, socket, settings, payload })
+    this.brokerEmitToSocket({ socket, settings, payload })
   }
 
   get broker () {
     return {
-      emit: this.brokerEmit.bind(this)
+      emit: this.brokerEmit.bind(this),
+      socket: {
+        emit: this.brokerEmitToSocket.bind(this)
+      },
+      topic: {
+        emit: this.brokerEmitToTopics.bind(this)
+      }
     }
   }
 }
