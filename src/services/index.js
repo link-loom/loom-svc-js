96ef35ee8a7f928dd26921448232432f67df8843
sebastian hoyos <sebastian.h.piedrahita@gmail.com>
2023-09-27 16:01:40 -0500

    feat: Implement Password Recovery & Refactor Auth Services
    - Introduced `/password/email/reset` and `/password/email/new` endpoints for password recovery:
      - `password/email/reset` generates a token and emails a recovery link to users.
      - `password/email/new` validates the token and allows password resetting.
    
    - Removed `auth` services and routes; authentication is now centralized within the `security` service.
    
    - Updated `notifications` service to send password recovery emails with a Spanish template.

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,4 @@
-const AuthService = require('./auth/auth.service');
+const SecurityAuthPasswordService = require('./security/auth-password/auth-password.service');
 const RemoteApiService = require('./remote-api/remote-api.service');
 const ApiManagerService = require('./api-manager/api-manager.service');
 const HealthService = require('./health/health.service');
@@ -11,7 +11,7 @@ const UserService = require('./user/user-management/user-management.service');
 const Template = require('./_template/_template.service');
 
 module.exports = {
-  AuthService,
+  SecurityAuthPasswordService,
   RemoteApiService,
   DeviceService,
   NotificationService,
